b5b474f74bd784d295ed4aac7a640d514013cd15
singhPriyanka17 <priyankasinghja17@gmail.com>
2024-05-06 23:58:56 +0530

    basic api setup

---
+++ src/controllers/pingController.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express"
+
+export const pingCheck = (_req: Request, res: Response) => {
+    return res.status(200).json({
+        message: "Ping Check on"
+    })
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express'
+
 import serverConfig from './config/serverConfig'
+import apiRouter from './routes'
 
 const app = express()
+
+app.use('/api', apiRouter)
 
 app.listen(serverConfig.PORT, ()=>{
     console.log(`server started at *:${serverConfig.PORT}`)
